5ee79a1f4e1203d6d4cc2f2528caaf695b539d16
unalcama35 <ucama20@ku.edu.tr>
2023-07-13 10:45:10 +0300

    home and generate component init

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { GenerateComponent } from './components/generate/generate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'generate', component: GenerateComponent},
+
+
+  {path:'**', redirectTo: 'home', pathMatch: 'full' }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
